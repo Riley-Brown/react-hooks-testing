6a847e39d1098bfedb51d9f2a4db5fbd12c5e5f2
Riley-Brown <riley@riley.gg>
2019-04-19 23:20:37 -0400

    mapping over and displaying todos

--- src/Todo.js
+++ src/Todo.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export default function Todo() {
-  return (
-    <div>
-      <h2>Todos map will go here</h2>
-    </div>
+export default function Todo(props) {
+  console.log(props);
+  const todo = props.todos.map(
+    todo => (
+      console.log(todo),
+      (
+        <div className="todo">
+          <h2>{todo.title}</h2>
+          <p>{todo.body}</p>
+        </div>
+      )
+    )
   );
+  return todo;
 }
